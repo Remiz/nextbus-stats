2fd214e65df58832027ffdec7bb2449a1190010a
Remi <remi@suggestar.com>
2015-12-15 17:08:31 -0500

    New app to store Routes and Stops. Added migration to populate these with TTC routes/stops.

---
+++ nextbusstats/routes/models.py
@@ -0,0 +1,23 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+class Route(models.Model):
+    tag = models.CharField(max_length=10)
+    title = models.CharField(max_length=100)
+    color = models.CharField(max_length=6)
+    opposite_color = models.CharField(max_length=6)
+    lat_min = models.FloatField()
+    lat_max = models.FloatField()
+    lon_min = models.FloatField()
+    lon_max = models.FloatField()
+
+
+class Stop(models.Model):
+    tag = models.CharField(max_length=10)
+    stop_id = models.CharField(max_length=10, null=True)
+    title = models.CharField(max_length=200)
+    lat = models.FloatField()
+    lon = models.FloatField()
+    route = models.ForeignKey('Route', on_delete=models.CASCADE, related_name='stops')
--- nextbusstats/settings.py
+++ nextbusstats/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # custom apps
     'nextbusstats.nextbus',
+    'nextbusstats.routes',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -121,5 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#######
+# App custom settings
+#######
+AGENCY_TAG = 'ttc'
 
 from settings_secret import *
